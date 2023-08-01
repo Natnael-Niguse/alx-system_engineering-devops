155ec7eac292bd14ac690e3f16ea745f32e6d86f
MNatiki <natiart07@gmail.com>
2023-08-01 15:04:51 -0700

    add

---
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+# Ensure there is an argument provided
+if ARGV.empty?
+  puts "Usage: #{$PROGRAM_NAME} <string>"
+  exit 1
+end
+
+# Get the argument passed to the script
+input_string = ARGV[0]
+
+# Define the regular expression pattern to match "t" with 2 to 5 repetitions between "h" and "n"
+pattern = /hb?t?n/
+
+# Perform the regex scan on the input string
+matches = input_string.scan(pattern)
+
+# Output the matched results, if any, on separate lines
+puts matches.join("\n")
