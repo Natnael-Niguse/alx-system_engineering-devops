92747df941fb7d358fd14d1738ec83a14f779ff1
MNatiki <natiart07@gmail.com>
2023-08-01 15:34:40 -0700

    ADD

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,19 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:(.*9)\] \[to:(.*9)\] \[flags:(.*9)\]/).join(",") 
+
+# Ensure there is an argument provided
+if ARGV.empty?
+  puts "Usage: #{$PROGRAM_NAME} <string>"
+  exit 1
+end
+
+# Get the argument passed to the script
+input_string = ARGV[0]
+
+# Define the regular expression pattern to match the desired format
+pattern = /\[from:(\d*9)\] \[to:(\d*9)\] \[flags:(\d*9)\]/
+
+# Perform the regex scan on the input string
+matches = input_string.scan(pattern)
+
+# Output the matched results, if any, separated by commas
+puts matches.join(",")
