2a42c5d792d39af5f95e901d56741323119e8cbe
MNatiki <natiart07@gmail.com>
2023-11-08 03:42:01 -0800

    api

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,18 +1,18 @@
+#!/usr/bin/python3
+'''Get top 10 hot posts'''
+import pprint
 import requests
-'''Get and print the titles of the first 10 hot posts'''
 
 BASE_URL = 'http://reddit.com/r/{}/hot.json'
 
 
 def top_ten(subreddit):
-    '''Get and print the titles of the first 10 hot.'''
+    '''Get top 10 hot posts'''
     headers = {'User-agent': 'Unix:0-subs:v1'}
     response = requests.get(BASE_URL.format(subreddit),
-                            headers=headers, allow_redirects=False)
-
-    if response.status_code == 200:
-        data = response.json().get('data', {}).get('children', [])
-        for post in data[0:10]:
-            print(post.get('data', {}).get('title'))
-    else:
-        print("None")
+                            headers=headers)
+    data = response.json().get('data', {}).get('children', {})
+    if response.status_code != 200 or not data:
+        return print("None")
+    for post in data[0:10]:
+        print(post.get('data', {}).get('title'))
