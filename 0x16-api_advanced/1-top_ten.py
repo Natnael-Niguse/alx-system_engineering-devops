13ff5be83616542231f3e98640626db07480ceab
MNatiki <natiart07@gmail.com>
2023-11-08 03:41:03 -0800

    api

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,10 +3,12 @@ import requests
 
 BASE_URL = 'http://reddit.com/r/{}/hot.json'
 
+
 def top_ten(subreddit):
     '''Get and print the titles of the first 10 hot.'''
     headers = {'User-agent': 'Unix:0-subs:v1'}
-    response = requests.get(BASE_URL.format(subreddit), headers=headers, allow_redirects=False)
+    response = requests.get(BASE_URL.format(subreddit),
+                            headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
         data = response.json().get('data', {}).get('children', [])
